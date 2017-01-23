759fb4cdb2893974435903f768899760b89f2a57
QPCA <qpca@outlook.com>
2017-01-23 08:37:19 +0700

    Basic classes added

---
+++ Filler/Filler/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Filler
+{
+    
+
+
+    class Player
+    {
+        
+    }
+
+    class Field
+    {
+        const int width = 24;
+        const int height = 20;
+        private int[][] field;
+
+        public Field()
+        {
+            field = new int[height][];
+            for (int i = 0; i < height; i++)
+                field[i] = new int[width];
+        }
+
+        private void Fill()
+        {
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+
+                }
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
